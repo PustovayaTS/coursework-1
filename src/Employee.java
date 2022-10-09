a34d6cacf4a4b53eed3f9ef61560a8f1546ec403
PustovayaTS <ais_ta@bk.ru>
2022-10-09 18:46:47 +0700

    Курсовая. Базовая сложность.

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+public class Employee {
+    private String fullName;
+    private int department;
+    private int salary;
+    private int empId;
+    private static int id;
+
+    public Employee(String fullName, int department, int salary) {
+        id++;
+        this.empId = id;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public int getEmpId() {
+        return this.empId;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void getEmpList() {
+        System.out.println(this.empId);
+    }
+
+    public String toString() {
+        return "id: " + empId + " ФИО: " + fullName + " Отдел: " + department + " Зарплата: " + salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,96 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Main {
+    public static Employee[] empStore = new Employee[10];
+
+    public static void main(String[] args) {
+        empStore[0]= new Employee("Name1 Surname1 Patronymic1",1,100000);
+        empStore[1] = new Employee("Name2 Surname2 Patronymic2",2,120000);
+        empStore[2] = new Employee("Name3 Surname3 Patronymic3",5,190000);
+        empStore[3] = new Employee("Name4 Surname4 Patronymic4",4,170000);
+        empStore[4] = new Employee("Name5 Surname5 Patronymic5",3,150000);
+        printEmpList(empStore);
+        System.out.println("Сумма затрат на зарплаты в месяц: " + salaryCosts(empStore) + " рублей.");
+        System.out.println("Сотрудник с минимальной зарплатой: " + minSalaryEmp(empStore).toString());
+        System.out.println("Сотрудник с максимальной зарплатой: " + maxSalaryEmp(empStore).toString());
+        System.out.println("Среднее значение зарплат: " + averageSalary(empStore));
+        printFullName(empStore);
+    }
+
+    // Получить список всех сотрудников со всеми имеющимися по ним данными
+    public static void printEmpList(Employee[] empList){
+        for (int i = 0; i < empList.length; i++){
+            if (empList[i] != null) {
+                System.out.println(empList[i].toString());
+            }
+        }
+    }
+
+    // Посчитать сумму затрат на зарплаты в месяц
+    public static int salaryCosts(Employee[] empList) {
+        int totalSalary = 0;
+        for (int i = 0; i < empList.length; i++) {
+            if (empList[i] != null) {
+                totalSalary = totalSalary + empList[i].getSalary();
+            }
+        }
+        return totalSalary;
+    }
+
+    // Найти сотрудника с минимальной зарплатой
+    public static Employee minSalaryEmp(Employee[] empList) {
+        int minSalary = 0;
+        int minSalaryEmpInd = 0;
+        for (int i = 0; i < empList.length; i++) {
+            if (empList[i] != null) {
+                if (minSalary == 0) { //Если empList[0] не заполнен
+                    minSalary = empList[i].getSalary();
+                }
+                if (minSalary > empList[i].getSalary()) {
+                    minSalary = empList[i].getSalary();
+                    minSalaryEmpInd = i;
+                }
+            }
+        }
+        Employee minSalaryEmp = empList[minSalaryEmpInd];
+        return minSalaryEmp;
+    }
+
+    // Найти сотрудника с максимальной зарплатой
+    public static Employee maxSalaryEmp(Employee[] empList) {
+        int maxSalary = 0;
+        int maxSalaryEmpInd = 0;
+        for (int i = 0; i < empList.length; i++) {
+            if (empList[i] != null) {
+                if (maxSalary < empList[i].getSalary()) {
+                    maxSalary = empList[i].getSalary();
+                    maxSalaryEmpInd = i;
+                }
+            }
+        }
+        Employee maxSalaryEmp = empList[maxSalaryEmpInd];
+        return maxSalaryEmp;
+    }
+
+    // Подсчитать среднее значение зарплат
+    public static int averageSalary(Employee[] empList) {
+        int totalSalary = 0;
+        int totalCnt = 0;
+        for (int i = 0; i < empList.length; i++) {
+            if (empList[i] != null) {
+                totalSalary = totalSalary + empList[i].getSalary();
+                totalCnt++;
+            }
+        }
+        return totalSalary/totalCnt;
+    }
+
+    // Получить Ф. И. О. всех сотрудников
+    public static void printFullName(Employee[] empList) {
+        for (int i = 0; i < empList.length; i++){
+            if (empList[i] != null) {
+                System.out.println(empList[i].getFullName());
+            }
+        }
     }
 }
